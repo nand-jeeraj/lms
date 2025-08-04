aef43f3e8e68a2a8a91c4b0feadd74293ba93f92
nandana <nandanajeeraj@gmail.com>
2025-08-04 16:39:43 +0530

    update api instance

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = "http://127.0.0.1:8000/";
+export const BASE_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || BASE_URL,  
