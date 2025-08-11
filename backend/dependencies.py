83a4137710c5801b0fe795e8c22164114b24e46c
Nandana Jeeraj <nandanajeeraj@gmail.com>
2025-08-11 11:26:32 +0530

    Delete backend directory

--- backend/dependencies.py
+++
@@ -1,5 +0,0 @@
-from flask import g, abort
-
-def get_current_user():
-    # Replace this with real authentication later (JWT, session, etc.)
-    return {"id": "Faculty123", "username": "Faculty"}
--- backend/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_pymongo import PyMongo
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS
-
-mongo = PyMongo()
-bcrypt = Bcrypt()
-jwt = JWTManager()
-cors = CORS()
