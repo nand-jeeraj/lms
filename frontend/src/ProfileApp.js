9d8496d2f7c9343dd31a7e5c4bf31cd9a9ef30bb
Nandana Jeeraj <nandanajeeraj@gmail.com>
2025-08-11 11:26:44 +0530

    Delete frontend directory

--- frontend/src/ProfileApp.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProfilePage from "./profile/ProfilePage";
-
-function ProfileApp() {
-  return <ProfilePage />;
-}
-
-export default ProfileApp;
--- frontend/src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-// src/utils/auth.js
-export const checkAuth = () => {
-  const token = localStorage.getItem('token');
-  const role = localStorage.getItem('role');
-  return {
-    isAuthenticated: !!token,
-    role: role || 'Student'
-  };
-};
